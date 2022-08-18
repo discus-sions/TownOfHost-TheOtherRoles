dedabfcc3d533c2e3fe77543b3643b97a0554b9f
music-discussion <80537729+music-discussion@users.noreply.github.com>
2022-08-18 13:59:50 -0500

    Jester Venting

--- Patches/UsablesPatch.cs
+++ Patches/UsablesPatch.cs
@@ -48,6 +48,8 @@ namespace TownOfHost
                 canUse = couldUse = VentForTrigger = true;
             else if (pc.Object.Is(CustomRoles.Jackal))
                 canUse = couldUse = Options.JackalCanVent.GetBool();
+            else if (pc.Object.Is(CustomRoles.Jester))
+                canUse = couldUse = Options.JesterCanVent.GetBool();
             else if (pc.Object.Is(CustomRoles.Pestilence))
                 canUse = couldUse = Options.PestiCanVent.GetBool();
             else if (pc.Role.TeamType == RoleTeamTypes.Impostor || pc.Role.Role == RoleTypes.Engineer) // インポスター陣営ベースの役職とエンジニアベースの役職は常にtrue
