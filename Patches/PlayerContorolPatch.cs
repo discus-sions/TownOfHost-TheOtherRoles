1a7318fb06bd00d1b6f494b4d642842b68d16f3c
music-discussion <80537729+music-discussion@users.noreply.github.com>
2022-08-30 23:46:24 -0500

    neutral killings cannot turn into skmm

--- Patches/PlayerContorolPatch.cs
+++ Patches/PlayerContorolPatch.cs
@@ -1096,7 +1096,7 @@ namespace TownOfHost
                 float dis;
                 foreach (PlayerControl p in PlayerControl.AllPlayerControls)
                 {
-                    if (!p.Data.IsDead && p.Data.Role.Role != RoleTypes.Shapeshifter && !p.Is(RoleType.Impostor) && !p.Is(CustomRoles.SKMadmate))
+                    if (!p.Data.IsDead && p.Data.Role.Role != RoleTypes.Shapeshifter && !p.Is(RoleType.Impostor) && !p.Is(CustomRoles.SKMadmate) && !p.GetCustomRole().IsNeutralKilling())
                     {
                         dis = Vector2.Distance(shapeshifterPosition, p.transform.position);
                         mpdistance.Add(p, dis);
