4a91dcac1b939d80193b7263183f4b921704af59
music-discussion <80537729+music-discussion@users.noreply.github.com>
2022-09-03 17:05:09 -0500

    disabled the 2 broken roles rn

--- Modules/OptionHolder.cs
+++ Modules/OptionHolder.cs
@@ -409,9 +409,9 @@ namespace TownOfHost
             SetupRoleOptions(2000, CustomRoles.Puppeteer);
             Mare.SetupCustomOption();
             TimeThief.SetupCustomOption();
-            Camouflager.SetupCustomOption();
+            //Camouflager.SetupCustomOption();
             Ninja.SetupCustomOption();
-            SetupSingleRoleOptions(2600, CustomRoles.Silencer, 1);
+            //SetupSingleRoleOptions(2600, CustomRoles.Silencer, 1);
             SilenceCooldown = CustomOption.Create(2610, Color.white, "SilenceDelay", 30, 2.5f, 180, 2.5f, CustomRoleSpawnChances[CustomRoles.Silencer]);
             //SetupSingleRoleOptions(2700, CustomRoles.CorruptedSheriff, 1);
             //PlayersForTraitor = CustomOption.Create(2710, Color.white, "TraitorSpawn", 1, 0, 15, 1, CustomRoleSpawnChances[CustomRoles.CorruptedSheriff]);
