cf9e3b468066b878d61caaad52f724889315d946
music-discussion <80537729+music-discussion@users.noreply.github.com>
2022-09-09 17:01:04 -0500

    0.8.2

--- Patches/ChatCommandPatch.cs
+++ Patches/ChatCommandPatch.cs
@@ -819,11 +819,12 @@ namespace TownOfHost
                 case "/level":
                     if (Options.Customise.GetBool())
                     {
-                        subArgs = args.Length < 2 ? "" : args[1];
+                        /*subArgs = args.Length < 2 ? "" : args[1];
                         Utils.SendMessage("Current AU Level Set to " + subArgs + ". AU auto adds 1 to your current level. Starting players are at level 0, so AU adds 1 to make you level 1. So no one is level 100, we are all just at level 99.", player.PlayerId);
                         //nt32.Parse("-105");
                         var number = System.Convert.ToUInt32(subArgs);
-                        player.RpcSetLevel(number);
+                        player.RpcSetLevel(number);*/
+                        Utils.SendMessage("This command currently does not work as intended for non-host players.\nIn order to prevent kicks, we have disabled this command.", player.PlayerId);
                     }
                     else { Utils.SendMessage("The host has currently disabled access to this command.\nTry again when this command is enabled.", player.PlayerId); }
                     break;
